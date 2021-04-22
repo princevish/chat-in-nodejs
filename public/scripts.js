c7b9842ce7f59590600db01e8028e09f499c4390
Prince Vishwakarma <princevworld@gmail.com>
2021-04-22 19:54:22 +0530

    t

--- public/scripts.js
+++ public/scripts.js
@@ -8,7 +8,7 @@ $('#buttons').click(() => {
     msg: $('#inputfield').val().trim()
   })
 
-  $('#massageboxf').append($('<p>').text($('#inputfield').val().trim()).attr('class', 'text-right bg-success msg mb-2'))
+  $('#massageboxf').append($('<p>').text($('#inputfield').val().trim()).attr('class', 'text-right bg-success msg-right'))
 
   $('#inputfield').val(" ")
   scrollToBottom()
@@ -18,7 +18,7 @@ socket.on('msg_rcvd', (data) => {
   var massage = `${data.msg}`
   var username = `${data.users}`
   $('#massageboxf').append($('<p>').text(username).attr('class', 'username'))
-  $('#massageboxf').append($('<p>').text(massage).attr('class', 'text-left bg-primary msg mb-2'))
+  $('#massageboxf').append($('<p>').text(massage).attr('class', 'text-left bg-primary msg-left'))
   scrollToBottom()
 })
 
