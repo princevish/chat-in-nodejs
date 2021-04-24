568d0e0827e6f7146e7546ee120315f3734633fe
Prince Vishwakarma <princevworld@gmail.com>
2021-04-24 15:15:48 +0530

    adding ip

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const socketio = require('socket.io')
-const http = require('http')
-const port = process.env.PORT || 3000;
+const socketio = require('socket.io');
+const http = require('http');
+const port = process.env.PORT || 8080;
+const ip =process.env.IP || '0.0.0.0';
 const app = express();
 
 const server = http.createServer(app)
 const io = socketio(server)
 
 io.on('connection', (socket) => {
-    console.log('connected with socket id =', socket.id)
+    
     socket.on('msg_send', (data) => {
 
         socket.broadcast.emit('msg_rcvd', data)
@@ -18,7 +19,7 @@ io.on('connection', (socket) => {
 })
 
 app.use('/', express.static(__dirname + '/public'))
-server.listen(port, function (error) {
+server.listen(port,ip, function (error) {
     if (error) {
         console.log("error in running the server", error);
     } else {
