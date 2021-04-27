c493f8e749b1c0f351ce793b6b2fa64aacfcb049
Prince Vishwakarma <princevworld@gmail.com>
2021-04-27 15:37:59 +0530

    add online user

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 const express = require('express');
+const handlebars = require('handlebars')
 const socketio = require('socket.io');
 const http = require('http');
+const path = require("path");
 const port = process.env.PORT || 8080;
-const ip =process.env.IP || '0.0.0.0';
+const ip = process.env.IP || '0.0.0.0';
 const app = express();
 
-const server = http.createServer(app)
-const io = socketio(server)
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "hbs");
+app.use('/file', express.static(__dirname + '/public'));
+app.use(express.json());
+const users = [];
 
 io.on('connection', (socket) => {
-    
+    function chatroom(s, u) {
+        s.join(socket.id)
+        users.push({
+            "id": socket.id,
+            "user": u
+        });
+        socket.broadcast.emit('online', users) 
+    }
+    socket.on('adduser', (data) => {
+        chatroom(socket, data)
+    });
     socket.on('msg_send', (data) => {
+         if (data.to){
+             io.to(data.to).emit('msg_rcvd', data)
+         }else{
+             socket.broadcast.emit('msg_rcvd', data)
+         }
+    });
 
-        socket.broadcast.emit('msg_rcvd', data)
+    socket.on('disconnect', function () {
+        for (let x in users) {
+        if(users[x].id == socket.id){
+            
+            users.splice(x,  1);
+        }}   
+        socket.broadcast.emit('online', users)
+    });
 
-    })
+});
 
-})
+app.get('/', function (req, res) {
+    res.render('index')
+});
 
-app.use('/', express.static(__dirname + '/public'))
-server.listen(port,ip, function (error) {
+server.listen(port, ip, function (error) {
     if (error) {
         console.log("error in running the server", error);
     } else {
-        console.log(`server is running on the port:${port}` );
+        console.log(`server is running on the port:${port}`);
     }
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,37 +1,82 @@
 let socket = io()
+let list = new Set();
+let a = 0;
+var to;
+
+function onlineuser() {
+  socket.on('online', (data) => {
+ $('option').remove()
+ $('#chatonline').append(new Option('All User',''))
+    for (let x in data) {
+
+      let id = data[x].id
+      let user = data[x].user
+      list.add(id)
+
+      if (list.has(`${id}`)) {
+       
+        $('#chatonline').append(new Option(user, id))
+      }
+    }
+  })
+}
+
+
 do {
   var user = prompt("Enter username")
 } while (!user)
-function addmassage(){
+
+socket.emit('adduser', user)
+
+
+$('#chatonline').change(function () {
+  var $option = $(this).find('option:selected');
+  var value = $option.val();
+  var text = $option.text();
+  to = value
+});
+
+
+function addmassage(to) {
+
   socket.emit('msg_send', {
+    to: to,
     users: user,
     msg: $('#inputfield').val().trim()
   })
 
-  $('#massageboxf').append($('<div>').attr('id','mass-right').append($('<spam>').text($('#inputfield').val().trim())))
+  $('#massageboxf').append($('<div>').attr('id', 'mass-right').append($('<spam>').text($('#inputfield').val().trim())))
 
   $('#inputfield').val(" ")
   scrollToBottom()
 }
 $('#buttons').click(() => {
-  addmassage()
-})
+
+  addmassage(to)
+
+});
+
+
+
+
 
 socket.on('msg_rcvd', (data) => {
   var massage = `${data.msg}`
   var username = `${data.users}`
   $('#massageboxf').append($('<p>').text(username).attr('class', 'username'))
-  $('#massageboxf').append($('<div>').attr('id','mass-left').append($('<spam>').text(massage)))
+  $('#massageboxf').append($('<div>').attr('id', 'mass-left').append($('<spam>').text(massage)))
   scrollToBottom()
 })
 
-function scrollToBottom(){
-  massageboxf.scrollTop=massageboxf.scrollHeight
+function scrollToBottom() {
+  massageboxf.scrollTop = massageboxf.scrollHeight
 }
 $('#inputfield').keypress(function (e) {
   var key = e.which;
-  if(key == 13)  // the enter key code
-   {
+  if (key == 13) // the enter key code
+  {
     addmassage()
-   }
- });   
+  }
+});
+
+onlineuser()
