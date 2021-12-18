48f9eb265f0cc1476430ba332b8095daf19cf7d6
Prince Vishwakarma <princevworld@gmail.com>
2021-12-18 17:38:13 +0530

    Add Image upload

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 const port = process.env.PORT || 8080;
-const ip = process.env.IP || '0.0.0.0';
+const ip = process.env.IP || "0.0.0.0";
 const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "hbs");
-app.use('/file', express.static(__dirname + '/public'));
+app.use("/file", express.static(__dirname + "/public"));
 app.use(express.json());
 const users = [];
 
-io.on('connection', (socket) => {
-    function chatroom(s, u) {
-        s.join(socket.id)
-        users.push({
-            "id": socket.id,
-            "user": u
-        });
-        socket.broadcast.emit('online', users) 
+io.on("connection", (socket) => {
+  function chatroom(s, u) {
+    s.join(socket.id);
+    users.push({
+      id: socket.id,
+      user: u,
+    });
+    socket.broadcast.emit("online", users);
+  }
+  socket.on("adduser", (data) => {
+    chatroom(socket, data);
+  });
+  socket.on("msg_send", (data) => {
+    if (data.to) {
+      io.to(data.to).emit("msg_rcvd", data);
+    } else {
+      socket.broadcast.emit("msg_rcvd", data);
     }
-    socket.on('adduser', (data) => {
-        chatroom(socket, data)
-    });
-    socket.on('msg_send', (data) => {
-         if (data.to){
-             io.to(data.to).emit('msg_rcvd', data)
-         }else{
-             socket.broadcast.emit('msg_rcvd', data)
-         }
-    });
+  });
 
-    socket.on('disconnect', function () {
-        for (let x in users) {
-        if(users[x].id == socket.id){
-            
-            users.splice(x,  1);
-        }}   
-        socket.broadcast.emit('online', users)
-    });
-
+  socket.on("disconnect", function () {
+    for (let x in users) {
+      if (users[x].id == socket.id) {
+        users.splice(x, 1);
+      }
+    }
+    socket.broadcast.emit("online", users);
+  });
 });
 
-app.get('/', function (req, res) {
-    res.render('index')
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
 server.listen(port, ip, function (error) {
-    if (error) {
-        console.log("error in running the server", error);
-    } else {
-        console.log(`server is running on the port:${port}`);
-    }
+  if (error) {
+    console.log("error in running the server", error);
+  } else {
+    console.log(`server is running on the port:${port}`);
+  }
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,84 +1,121 @@
-let socket = io()
+let socket = io();
 let list = new Set();
 let a = 0;
 var to;
 
+let imagefile = document.getElementById("imagefile");
 function onlineuser() {
-  socket.on('online', (data) => {
- $('option').remove()
- $('#chatonline').append(new Option('All User',''))
+  socket.on("online", (data) => {
+    $("option").remove();
+    $("#chatonline").append(new Option("All User", ""));
     for (let x in data) {
-
-      let id = data[x].id
-      let user = data[x].user
-      list.add(id)
+      let id = data[x].id;
+      let user = data[x].user;
+      list.add(id);
 
       if (list.has(`${id}`)) {
-       
-        $('#chatonline').append(new Option(user, id))
+        $("#chatonline").append(new Option(user, id));
       }
     }
-  })
+  });
 }
-
 
 do {
-  var user = prompt("Enter username")
-} while (!user)
+  var user = prompt("Enter username");
+} while (!user);
 
-socket.emit('adduser', user)
+socket.emit("adduser", user);
 
-
-$('#chatonline').change(function () {
- 
-  var $option = $(this).find('option:selected');
+$("#chatonline").change(function () {
+  var $option = $(this).find("option:selected");
   var value = $option.val();
   var text = $option.text();
-  to = value
-  $('#massageboxf').append($('<div>').attr('class', 'text-center text-primary').text(`--------- ${text} ---------`))
+  to = value;
+  $("#massageboxf").append(
+    $("<div>")
+      .attr("class", "text-center text-primary")
+      .text(`--------- ${text} ---------`)
+  );
 });
-
 
 function addmassage(to) {
-
-  socket.emit('msg_send', {
+  if (file) {
+    socket.emit("msg_send", {
+      to: to,
+      users: user,
+      file: file,
+    });
+  }
+  socket.emit("msg_send", {
     to: to,
     users: user,
-    msg: $('#inputfield').val().trim()
-  })
+    msg: $("#inputfield").val().trim(),
+  });
 
-  $('#massageboxf').append($('<div>').attr('id', 'mass-right').append($('<spam>').text($('#inputfield').val().trim())))
-  scrollToBottom()
-  $('#inputfield').val(" ")
-
+  $("#massageboxf").append(
+    $("<div>")
+      .attr("id", "mass-right")
+      .append($("<spam>").text($("#inputfield").val().trim()))
+  );
+  scrollToBottom();
+  $("#inputfield").val(" ");
+  file = "";
 }
-$('#buttons').click(() => {
-
-  addmassage(to)
-
+$("#buttons").click(() => {
+  addmassage(to);
 });
 
-
-
-
-
-socket.on('msg_rcvd', (data) => {
-  var massage = `${data.msg}`
-  var username = `${data.users}`
-  $('#massageboxf').append($('<p>').text(username).attr('class', 'username'))
-  $('#massageboxf').append($('<div>').attr('id', 'mass-left').append($('<spam>').text(massage)))
-  scrollToBottom()
-})
+socket.on("msg_rcvd", (data) => {
+  var massage = `${data.msg}`;
+  var username = `${data.users}`;
+  if (data.file) {
+    ImageShow(data.file, false);
+  } else {
+    $("#massageboxf").append($("<p>").text(username).attr("class", "username"));
+    $("#massageboxf").append(
+      $("<div>").attr("id", "mass-left").append($("<spam>").text(massage))
+    );
+  }
+  scrollToBottom();
+});
 
 function scrollToBottom() {
-  massageboxf.scrollTop = massageboxf.scrollHeight
+  massageboxf.scrollTop = massageboxf.scrollHeight;
 }
-$('#inputfield').keypress(function (e) {
+$("#inputfield").keypress(function (e) {
   var key = e.which;
-  if (key == 13) // the enter key code
-  {
-    addmassage()
+  if (key == 13) {
+    // the enter key code
+    addmassage();
   }
 });
 
-onlineuser()
+onlineuser();
+
+function ImageShow(filedata, position) {
+  var file = new Blob([filedata], { type: filedata.type });
+  var img = document.createElement("img");
+  img.alt = filedata.name;
+  img.id = "image";
+  const render = new FileReader();
+  render.readAsDataURL(file);
+  render.onloadend = function () {
+    img.src = render.result;
+  };
+  if (position) {
+    $("#massageboxf").append(
+      $("<div>").attr("id", "imageDivRight").append(img)
+    );
+  } else {
+    $("#massageboxf").append($("<div>").attr("id", "imageDivLeft").append(img));
+  }
+}
+
+function onSelectFile({ target: { files } }) {
+  ImageShow(files[0], true);
+  socket.emit("msg_send", {
+    to: to,
+    users: user,
+    file: new Blob([files[0]], { type: files[0].type }),
+  });
+}
